247f2b35c9530f3f139cb8c8826c4b772a4412ca
Ali Heydari <aliheydari@zeiworks.com>
2018-04-16 15:15:21 -0700

    Modified Euler method (simplified Runge-Kutta) with user input
    This code is the modified Euler method (simplified Runge-Kutta) with user input

---
+++ mod_Euler_method.m
@@ -0,0 +1,41 @@
+% Modified Euler
+% here is a first try 
+% Alizei
+
+
+
+a = input('what is a?\n');
+b = input('what is b?\n');
+y_a = input('y(a) = ?\n');
+h = input('what is h?');
+%f = @(t,y) exp(t-y);
+f = input('what is y_prime?(type @(t,y) [then the function]');
+modi_euler(f,a,b,y_a,h)
+
+
+
+function E = modi_euler(f,a,b,y_a,h)
+    N = ceil( (b-a)/h ); 
+    % Compute N, rounding up to nearest integer if necessary
+    % changing this to be an array so we could use t_i and t_i+1 at the...
+    % ...same time
+    
+    t = a;
+    w = y_a;
+    E = zeros(N,2);
+    E(1,:) = [t w];
+    
+    for i=1:N
+        % here is the only change 
+        % for now
+        t = a + i*h; % Compute t_i
+        %compute t_i+1
+        q = a + (i-1)*h 
+       
+        w = w + (h/2) * (f(q,w) + f(t,w + h*(f(q,w)))); % Compute w_i
+        
+        E(i+1,:) = [t w];
+    end
+end
+
+
