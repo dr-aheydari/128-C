e79cf628d7236d550829dfeda2a9a5004939cbc6
Ali Heydari <aliheydari@zeiworks.com>
2018-05-23 23:12:17 -0700

    Updated Lotka-Volterra
    Updated the Lotka-Volterra code and added new screen shots

--- Project 3/First Computer Proj/lot_volt.m
+++ Project 3/First Computer Proj/lot_volt.m
@@ -5,40 +5,66 @@
 
 
 % timesteps
-tsteps = [0 40];
+tsteps = [0 .01];
 
 % col 1: rabbits, col 2: foxes
-init_vak = [300 150]; 
+init_vak = [500 500]; 
 
 % good ol' ODE solver
-[t, X] = ode45(@(t,X) lot_vot(t,X,0.01), tsteps, init_vak);
+% [t, X] = ode45(@(t,X) lot_vot(t,X,0.01), tsteps, init_vak);
 
 
 
 
 % plotting the phase plane
-figure;
-plot(X(:,1), X(:,2));
-xlabel('Rabbit');
-ylabel('Fox');
-grid on;
+% figure;
+% plot(X(:,1), X(:,2));
+% xlabel('Rabbit');
+% ylabel('Fox');
+% grid on;
+% 
+% title('ORIGINAL MODEL: Phase plane of Fox bs Rabbit when r_0 = 300, f_0 = 150');
+% 
 
-title('ORIGINAL MODEL: Phase plane of Fox bs Rabbit when r_0 = 300, f_0 = 150');
 
 
-
-
-% plot the population vs time 
-figure;
-plot(t, X(:,1), 'DisplayName', 'r(t)'); % Plot r(t) <- 1st col of X
-hold on;
-plot(t, X(:,2), 'DisplayName', 'f(t)'); % Plot f(t) <- 2nd col of X
-grid on;
+% plot the population vs time
+alphas = [ 5  10]; % Vector storing values of alphas
+% 
+  figure;
+% 
+ for i=1:length(alphas)
+% 
+[t, X] = ode45(@(t,X) lot_vot(t,X,alphas(i)), tsteps, init_vak);
+  subplot(length(alphas),1,i);
+% 
+ plot(t, X(:,1), 'DisplayName', 'r(t)');
+ hold on
+ plot(t, X(:,2), 'DisplayName', 'f(t)')
 xlabel('Time');
 ylabel('Population');
 legend('show')
+title(['Using alpha = ' num2str(alphas(i)) ' in Pop. vs Time']); %')
+% 
+% xlabel('Rabbit');
+% 
+% ylabel('Fox');
+% % how to do it : title(['y = sin(' num2str(i) 'x)'])
+%  title(['Using alpha = ' num2str(alphas(i))]); %')
+% 
+% grid on;
+% 
+ end
+% figure;
+% plot(t, X(:,1), 'DisplayName', 'r(t)'); % Plot r(t) <- 1st col of X
+% hold on;
+% plot(t, X(:,2), 'DisplayName', 'f(t)'); % Plot f(t) <- 2nd col of X
+% grid on;
+% xlabel('Time');
+% ylabel('Population');
+% legend('show')
 
-title('ORIGINAL MODEL: When r_0 = 300, f_0 = 150')
+% title('ORIGINAL MODEL: When r_0 = 300, f_0 = 150')
 
 % 
 % % this is for the first part where I compared the four alphas and 
@@ -50,7 +76,6 @@ title('ORIGINAL MODEL: When r_0 = 300, f_0 = 150')
 % 
 % for i=1:length(alphas)
 % 
-% [t, X] = ode45(@(t,X) ecosystem(t,X,alphas(i)),ts,IC); % Solve the system for given alpha
 % 
 % subplot(4,1,i);
 % 
